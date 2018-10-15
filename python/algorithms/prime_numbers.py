a15d9870ecf2964342fc6f7382ea6ef1723c0338
Robert Barron <rmbarron@google.com>
2018-10-15 16:33:37 -0700

    Add erastothenes optimizations and edge case checks

--- python/algorithms/prime_numbers.py
+++ python/algorithms/prime_numbers.py
@@ -5,20 +5,27 @@ Problem Statement: Find nth prime number given an index within the first 1000
 prime numbers.
 """
 
-from sys import argv
+import math
+import sys
 
 def erastothenes_sieve(position):
   """
   Find prime number at nth position by sieving away multiples of non-primes.
-  :param position: int index of prime to find
-  :return: int prime number at nth position
+  :param position: int index of prime to find.
+  :return: int prime number at nth position.
+  :raises: ValueError when position is a negative.
   """
+  if position < 1:
+    raise ValueError("Please provide a positive position.")
   # initialize with known base
   primes = [2, 3]
   counter = 4
   while len(primes) < position:
     is_prime = True
+    break_value = math.ceil(math.sqrt(counter))
     for i in primes:
+      if i > break_value:
+        break
       if (counter % i == 0):
         is_prime = False
         counter += 1
@@ -30,7 +37,11 @@ def erastothenes_sieve(position):
   return primes[position-1]
 
 def main():
-  print(erastothenes_sieve(int(argv[1])))
+  try:
+    print(erastothenes_sieve(int(sys.argv[1])))
+  except ValueError as e:
+    print(e)
+    sys.exit(1)
 
 if __name__=='__main__':
   main()
--- python/algorithms/prime_numbers_test.py
+++ python/algorithms/prime_numbers_test.py
@@ -8,11 +8,16 @@ class TestPrimeNumbers(unittest.TestCase):
 
   @parameterized.expand([
     ("position_5", 5, 11),
-    ("position_25", 25, 97)
+    ("position_25", 25, 97),
+    ("position_10,000", 10000, 104729)
   ])
   def test_erastothenes_sieve_at_known(self, name, position, expected):
     got = prime_numbers.erastothenes_sieve(position)
     self.assertEqual(got, expected)
+
+  def test_erastothenes_sieve_raises_ValueError(self):
+    with self.assertRaises(ValueError):
+      prime_numbers.erastothenes_sieve(-1)
 
 if __name__=='__main__':
   unittest.main()

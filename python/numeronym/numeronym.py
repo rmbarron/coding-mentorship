e7b7090facbea84c173cb36773c7c1e832700ca7
Robert Barron <rmbarron@google.com>
2018-10-16 15:47:25 -0700

    simple numeronym

---
+++ python/numeronym/numeronym.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Abbreviate a given word into a numeronym. A common example is
+"internationalization" becoming "i18n".
+"""
+
+from sys import argv
+
+def numeronym(base):
+  """
+  Reduce a given base word into a numeronym abbreviation.
+  :param base: string Word to abbreviate.
+  :return: string Abbreviation.
+  """
+  return "".join([base[0], str(len(base)-2), base[-1]])
+
+def main():
+  if len(argv) != 2:
+    print("Please supply only the word to be abbreviated.")
+  print(numeronym(argv[1]))
+
+if __name__=='__main__':
+  main()
---
+++ python/numeronym/numeronym_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import numeronym
+import unittest
+from parameterized import parameterized
+
+class TestNumeronym(unittest.TestCase):
+
+  @parameterized.expand([
+    ("word_internationalization", "internationalization", "i18n"),
+    ("word_localization", "localization", "l10n")
+  ])
+  def test_numeronym(self, name, base, expected):
+    got = numeronym.numeronym(base)
+    self.assertEqual(got, expected)
+
+if __name__=='__main__':
+  unittest.main()
